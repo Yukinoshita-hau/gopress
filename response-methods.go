8625757a9e00be2f4e8299fed1f55a9f8183a8f0
Yuki <Rashidmirzuev@yandex.ru>
2024-09-14 07:54:44 +0300

    added 2 new function

--- response-methods.go
+++ response-methods.go
@@ -2,16 +2,46 @@ package gopress
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 )
 
 type Response struct {
 	HttpResponse http.ResponseWriter
 }
 
-func (r Response) Json(data map[string]interface{}, statusCode int) {
-	r.HttpResponse.WriteHeader(statusCode)
+func (r Response) Json(data map[string]interface{}) {
 	if err := json.NewEncoder(r.HttpResponse).Encode(data); err != nil {
 		JsonErrorResponse(r, http.StatusInternalServerError, err.Error())
+	}
+}
+
+func (r Response) Download(path, name string) {
+	file, err := os.Open(path)
+	if err != nil {
+		JsonErrorResponse(r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer file.Close()
+	r.HttpResponse.Header().Set("Content-Disposition", "attachment; filename=\""+name+"\"")
+    r.HttpResponse.Header().Set("Content-Type", "application/octet-stream")
+	
+	if _, err := io.Copy(r.HttpResponse, file); err != nil {
+		JsonErrorResponse(r, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (r Response) SendFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		JsonErrorResponse(r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer file.Close()
+	if _, err := io.Copy(r.HttpResponse, file); err != nil {
+		JsonErrorResponse(r, http.StatusInternalServerError, err.Error())
+		return
 	}
 }
